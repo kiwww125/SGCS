813a51ec694558277ce2aa6fb269992aa202a042
Leegilwon <48116897+kiwww125@users.noreply.github.com>
2020-03-28 02:30:12 +0900

    Update week1_utoi.sh

--- CSE3013_Computer Science and Engineering laboratory I/homework/week1_utoi.sh
+++ CSE3013_Computer Science and Engineering laboratory I/homework/week1_utoi.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 
 echo "working directory:"
-
 read directory
-change=1
 
 if [ -z $directory ] 
 then 
 	cd $HOME
-	#echo "home directory"	
 else
 	if [ -d $directory -a -w $directory -a -r $directory ]
 	then
 		cd $directory
-		#echo "access to $directory"		
 	else 
 		echo "ERROR : cannot access given directory"
-		change=0
 	fi	
 fi
 
-if [ $change -eq 1 ]
-then
-	for filename in *
-	do
-		for ind in ${#filename}
-		do
-			if [ 'a' <= ${filename:ind:1} -io ${filename:ind:1} <= 'z' ] 
-				tr [:lower:] [:upper:] 
-					
-		done
-	done
-fi		
-
-
+for filename in *
+do
+	if [ -f $filename ]
+	then 
+		mv $filename filename | tr '[A-Z][a-z]' '[a-z][A-Z]'	
+	fi
+done
